05439e48e57ba29dcaa4f8c57e6b94878dadf59d
Pinal Bhatt <Pinal.Bhatt@weightwatchers.com>
2016-02-25 16:07:38 -0500

    commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,28 +18,6 @@ angular.module('starter.controllers', [])
 
 })
 
-  .controller('landingCtrl', function($scope, $rootScope, $state) {
-    $scope.selectMode = function(mode){
-      // 0 - WW Member
-      // 1 - Booster/Friend
-      $rootScope.mode = mode;
-      if(mode === 0){
-        $rootScope.user = {
-          type: "member",
-          name: "Jannifer"
-        };
-      }
-      else{
-        $rootScope.user = {
-          type: "booster",
-          name: "Json Saber"
-        };
-      }
-      $state.go("tab.dash");
-    }
-
-  })
-
 
   .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
