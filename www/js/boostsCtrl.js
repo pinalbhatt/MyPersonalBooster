600d8e9ee713765c44291be7a50fc57fa3d5bb40
elias.yousef <elias.yousef@weightwatchers.com>
2016-02-26 12:15:06 -0500

    fixing styling

--- www/js/boostsCtrl.js
+++ www/js/boostsCtrl.js
@@ -4,8 +4,8 @@ angular.module('starter.controllers')
       {name:"boost01",content : "Keep Going!",color:"#00A0DB"},
       {name:"boost02",content : "Keep up the amazing work!",color:"#F48C37"},
       {name:"boost03",content : "Stay strong and healthy :)",color:"#F19394"},
-      {name:"boost04",content : "❤️❤️❤️ GREAT JOB!!!❤️❤️❤️",color:"#FFF03A"},
-      {name:"boost05",content : "See u @ the meetong :)",color:"#F33333"},
+      {name:"boost04",content : "❤️❤️❤️ GREAT JOB!!! ❤️❤️❤️",color:"#FFF03A"},
+      {name:"boost05",content : "See u @ the meeting :)",color:"#F33333"},
       {name:"boost06",content : "Don’t give in to Temptation!!",color:"#2DC127"},
     ];
     $scope.custom = {};
@@ -13,7 +13,7 @@ angular.module('starter.controllers')
       $scope.user = $rootScope.member;
       $scope.show = false;
     });
-    
+
     /*$scope.user = {
      name: "Elias Yousef",
      theme: "do it!",
